9c8dc972fac8c1a222a2e1e0af00161f6199b81a
MarkusNodeJs <markus.dalton03282023@gmail.com>
2023-06-21 15:15:07 +0800

    register

--- controller/userController.js
+++ controller/userController.js
@@ -3,31 +3,41 @@ import bcrypt from "bcrypt";
 
 const db = con;
 
-// add user
-const addUser = async (req, res) => {
+//Register user
+const registerUser = async (req, res) => {
   const { Firstname, Lastname, Middlename, Position, Username, Password } =
     req.body;
   const hashpassword = await bcrypt.hash(Password, 10);
-  db.query(
-    `Insert into users SET ?`,
-    {
-      Firstname: Firstname,
-      Lastname: Lastname,
-      Middlename: Middlename,
-      Position: Position,
-      Username: Username,
-      Password: hashpassword,
-    },
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.status(200).json({
-          message: "Successfully Added",
-        });
-      }
+  const isResgisterUser = "Select * from users WHERE Username = ?";
+  db.query(isResgisterUser, Username, (err, result) => {
+    if (result != "") {
+      res.status(301).json({ message: "Username is already exist" });
+      console.log(result);
     }
-  );
+    if (result == "") {
+      db.query(
+        `Insert into users SET ?`,
+        {
+          Firstname: Firstname,
+          Lastname: Lastname,
+          Middlename: Middlename,
+          Position: Position,
+          Username: Username,
+          Password: hashpassword,
+        },
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.status(200).json({
+              message: "Successfully Added",
+            });
+            console.log("Successfully Added");
+          }
+        }
+      );
+    }
+  });
 };
 
 // get all user
@@ -90,4 +100,78 @@ const updateUser = async (req, res) => {
   );
 };
 
-export { addUser, getAllUser, getOneUser, deleteUser, updateUser };
+// const getResponse = async (req, res) => {
+//   const { id } = req.params;
+//   const {
+//     object,
+//     address,
+//     balance,
+//     created,
+//     currency,
+//     default_source,
+//     delinquent,
+//     description,
+//     discount,
+//     email,
+//     invoce_prefix,
+//     invoice_settings,
+//     custom_fields,
+//     default_payment_method,
+//     footer,
+//     rendering_options,
+//     livemode,
+//     metadata,
+//     orderid,
+//     name,
+//     next_invoce_sequence,
+//     phone,
+//     prefered_locales,
+//     shipping,
+//     tax_axemp,
+//     test_clock,
+//   } = req.body;
+
+//   const sql =
+//     "insert into stripe(object, address,balance,created,currency,default_source,delinquent,description,invoce_prefix,invoice_settings,custom_fields,default_payment_method,footer,rendering_options,livemode,metadata,orderid,name,next_invoce_sequence,phone,prefered_locales,shipping,tax_axemp,test_clock) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? ,? ,? ,? ,?";
+
+//   db.query(
+//     sql,
+//     [
+//       object,
+//       address,
+//       balance,
+//       created,
+//       currency,
+//       default_source,
+//       delinquent,
+//       description,
+//       invoce_prefix,
+//       invoice_settings,
+//       custom_fields,
+//       default_payment_method,
+//       footer,
+//       rendering_options,
+//       livemode,
+//       metadata,
+//       orderid,
+//       name,
+//       next_invoce_sequence,
+//       phone,
+//       prefered_locales,
+//       shipping,
+//       tax_axemp,
+//       test_clock,
+//     ],
+//     (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.status(200).json({
+//           message: "Success",
+//         });
+//       }
+//     }
+//   );
+// };
+
+export { registerUser, getAllUser, getOneUser, deleteUser, updateUser };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
 const router = express.Router();
 import {
-  addUser,
+  registerUser,
   getAllUser,
   getOneUser,
   deleteUser,
   updateUser,
 } from "../controller/userController.js";
 
-router.post("/user", addUser);
+router.post("/user", registerUser);
 router.get("/user", getAllUser);
 router.get("/user/:id", getOneUser);
 router.delete("/user/:id", deleteUser);
